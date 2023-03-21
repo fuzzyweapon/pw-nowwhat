87096acb78047d2593dff1ba5cb6e874e6034ad2
Julia <1253399+fuzzyweapon@users.noreply.github.com>
2023-03-20 17:39:02 -0700

    Fix using gui in server .bat

--- nw.server.bat
+++ nw.server.bat
@@ -1,1 +1,1 @@
-java -jar packwiz-installer-bootstrap.jar --bootstrap-update-url https://api.github.com/repos/fuzzyweapon/packwiz-installer/releases/latest -s server http://now-what.duckdns.org:8080/pack.toml
+java -jar packwiz-installer-bootstrap.jar --bootstrap-update-url https://api.github.com/repos/fuzzyweapon/packwiz-installer/releases/latest -g -s server http://now-what.duckdns.org:8080/pack.toml
